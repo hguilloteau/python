a018f6719747e77a890c666bd46b30c087a05184
Henri Guilloteau <henri.guilloteau@gmail.com>
2023-05-04 22:51:56 +0200

    Exercice 8.3

---
+++ exercice_8.3.py
@@ -0,0 +1,48 @@
+# Agrandissez le canevas de manière à lui donner une largeur de 500 unités et une hau- teur de 650. Modifiez également la taille des lignes, afin que leurs extrémités se confondent avec les bords du canevas.
+
+# Petit exercice utilisant la bibliothèque graphique tkinter
+from tkinter import *
+from random import randrange
+
+# --- définition des fonctions gestionnaires d'événements : ---
+
+
+def drawline():
+    "Tracé d'une ligne dans le canevas can1"
+    global x1, y1, x2, y2, coul
+    can1.create_line(x1, y1, x2, y2, width=2, fill=coul)
+    # modification des coordonnées pour la ligne suivante :
+    y2, y1 = y2-5, y1-5
+
+
+def changecolor():
+    "Changement aléatoire de la couleur du tracé"
+    # => génère un nombre aléatoire de 0 à 7
+    global coul
+    pal = ['purple', 'cyan', 'maroon', 'green',
+           'red', 'blue', 'orange', 'yellow']
+    c = randrange(8)
+    coul = pal[c]
+
+
+# ------ Programme principal -------
+# les variables suivantes seront utilisées de manière globale :
+# coordonnées de la ligne
+# x1, y1, x2, y2 = 10, 190, 190, 10
+# Modification des points d'init pour faire des lignes horizontales
+x1, y1, x2, y2 = 0, 500, 500, 500
+# couleur de la ligne
+coul = 'dark green'
+# Création du widget principal ("maître") :
+fen1 = Tk()
+# création des widgets "esclaves" :
+can1 = Canvas(fen1, bg='dark grey', height=500, width=500)
+can1.pack(side=LEFT)
+bou1 = Button(fen1, text='Quitter', command=fen1.quit)
+bou1.pack(side=BOTTOM)
+bou2 = Button(fen1, text='Tracer une ligne', command=drawline)
+bou2.pack()
+bou3 = Button(fen1, text='Autre couleur', command=changecolor)
+bou3.pack()
+fen1.mainloop()     # démarrage du réceptionnaire d’événements
+fen1.destroy()      # destruction (fermeture) de la fenêtre

7eca35058d57c8e9577a50657a6a6faaa7d5a3b0
Henri Guilloteau <henri.guilloteau@gmail.com>
2023-05-21 18:46:46 +0200

    Exercice 10.6

---
+++ exercice_10.6.py
@@ -0,0 +1,7 @@
+# Dans un conte américain, huit petits canetons s’appellent respectivement : Jack, Kack, Lack, Mack, Nack, Oack, Pack et Qack. Écrivez un petit script qui génère tous ces noms à partir des deux chaînes suivantes :
+# prefixes = 'JKLMNOP' et suffixe = 'ack'
+# Si vous utilisez une instruction for ... in ..., votre script ne devrait comporter que deux
+# lignes.
+
+for car in 'JKLMNOP':
+    print(car + 'ack')
